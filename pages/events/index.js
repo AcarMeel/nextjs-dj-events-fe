342e46f1fe8b213a080a821e09e2e1075f01a9c4
acarmeel <acarmeel@MacBook-Pro-de-acarmeel.local>
2021-06-21 18:57:52 -0600

    Pagination

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,9 @@
 import Layout from "@/components/Layout";
-import { API_URL } from "@/config/index";
+import { API_URL, PER_PAGE } from "@/config/index";
 import EventItem from "@/components/EventItem";
+import Pagination from "@/components/Pagination";
 
-export default function EventsPage({ events }) {
+export default function EventsPage({ events, page, total }) {
   return (
     <Layout>
       <h1>Events</h1>
@@ -10,17 +11,22 @@ export default function EventsPage({ events }) {
       {events.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
+      <Pagination page={page} total={total} />
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
-  const data = await res.json();
+export async function getServerSideProps({ query: { page = 1 }}) {
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
+  const totalRes = await fetch(`${API_URL}/events/count`);
+  const total = await totalRes.json();
+  const eventRes = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`);
+  const data = await eventRes.json();
   return {
     props: {
       events: data,
+      page: +page,
+      total
     },
-    revalidate: 1,
   };
 }
